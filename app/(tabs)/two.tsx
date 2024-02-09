0521eb3750f8166d27e0e402ae6a70e4922d2090
RaedRdhaounia <raedrdhaounia@gmail.com>
2024-02-09 11:48:41 +0100

    update structure project

---
+++ app/(tabs)/two.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '@/components/EditScreenInfo';
+import { View } from '@/components/Themed';
+import { MonoText } from '@/components/StyledText';
+
+export default function TabTwoScreen() {
+  return (
+    <View style={styles.container}>
+      <MonoText style={styles.title}>Tab Two</MonoText>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <EditScreenInfo path="app/(tabs)/two.tsx" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
