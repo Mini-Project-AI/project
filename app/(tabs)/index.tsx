82176859ffa0fb5f8fba36c75aab52e8d4dfccd2
RaedRdhaounia <raedrdhaounia@gmail.com>
2024-02-10 05:16:07 +0100

    test the server connection

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,30 @@
-import { StyleSheet } from 'react-native';
+import {useState} from "react"
+import { StyleSheet, TouchableOpacity, Alert, Platform } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
-import { View } from '@/components/Themed';
+import { Text, View} from '@/components/Themed';
 import { MonoText } from '@/components/StyledText';
+import webService from '@/server/useService';
 
 export default function TabOneScreen() {
+  const [message, setMessage]= useState("")
+  async function handleTest() {
+    try {
+      const response = await webService().testWebService()
+      if (response.status) {
+        setMessage(response.message)
+      }
+    } catch (error) {
+      console.error(error, "error")
+    }
+  }
   return (
     <View style={styles.container}>
+      <TouchableOpacity onPress={handleTest} >
+        <Text>
+        {message || "try to connect to the server"}
+        </Text>
+      </TouchableOpacity>
       <MonoText style={styles.title}>Tab One</MonoText>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
